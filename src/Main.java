55c42126296b71b2cec1429f62ef6d6df06f83c0
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-24 15:57:27 +0300

    Exercise 9

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        int firstNumber = Integer.parseInt(reader.nextLine());
         System.out.print("Type a number: ");
+        int firstNumber = Integer.parseInt(reader.nextLine());
 
-        int secondNumber = Integer.parseInt(reader.nextLine());
         System.out.print("Type another number: ");
+        int secondNumber = Integer.parseInt(reader.nextLine());
 
         System.out.println("Division: " + firstNumber + " / " + secondNumber + " = " + (firstNumber/secondNumber));
     }
