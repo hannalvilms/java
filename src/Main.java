c3a163ce6dbec6579629b590dfd8dc787936a65b
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-24 18:36:41 +0300

    Exercise 19

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.print("Type the points[0-60]: ");
-        int points = Integer.parseInt(reader.nextLine());
+        System.out.print("How old are you? ");
+        int age = Integer.parseInt(reader.nextLine());
 
-        if (points <= 29) {
-            System.out.println("Failed");
-        } else if (points >= 30 && points <= 34) {
-            System.out.println("Grade: 1");
-        } else if (points >= 35 && points <= 39) {
-            System.out.println("Grade: 2");
-        } else if (points >= 40 && points <= 44) {
-            System.out.println("Grade: 3");
-        } else if (points >= 45 && points <= 49) {
-            System.out.println("Grade: 4");
+        if (age >= 0 && age <= 120) {
+            System.out.println("OK!");
         } else {
-            System.out.println("Grade: 5");
+            System.out.println("Impossible!");
         }
 
     }
