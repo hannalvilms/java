6c8feeba8329ce36f67a857382493d79465e17cf
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-24 15:15:37 +0300

    Exercise 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hanna-Liisa Vilms");
+        int seconds = 60;
+        int minutes = 60;
+        int hours = 24;
+        int days = 265;
+        int secondsInAYear = seconds * minutes * hours * days;
+        System.out.println("There are " + secondsInAYear + " in a year.");
     }
 }
