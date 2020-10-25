bae4fdf8dfa5bba36b528c036bac1e7a6e2d60dd
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-25 21:00:26 +0200

    Exercise 37

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
-        System.out.print("Type numbers: ");
-        int number = Integer.parseInt(reader.nextLine());
-        int sum = 0;
-        int howMany = 0;
-        double average = 0;
-        int even = 0;
-        int odd = 0;
-
-        while (number != -1) {
-            if (number % 2 == 1) {
-                odd++;
-            } else {
-                even++;
-            }
-            sum += number;
-            howMany++;
-            average = (double)sum / howMany;
-            number = Integer.parseInt(reader.nextLine());
-        }
-        System.out.println("Even numbers: " + even);
-        System.out.println("Odd numbers: " + odd);
-        System.out.println("Average: " + average);
-        System.out.println("How many numbers: " + howMany);
-        System.out.println("The sum is: " + sum);
-        System.out.println("Thank you and see you later!");
+        printText();
+    }
+    public static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
     }
 }
