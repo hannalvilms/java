adba94ec8e8485d7cab1caf4141b01febe10d206
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-25 20:37:44 +0200

    Exercise 36.5

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,22 @@ public class Main {
         int sum = 0;
         int howMany = 0;
         double average = 0;
+        int even = 0;
+        int odd = 0;
 
         while (number != -1) {
+            if (number % 2 == 1) {
+                odd++;
+            } else {
+                even++;
+            }
             sum += number;
             howMany++;
             average = (double)sum / howMany;
             number = Integer.parseInt(reader.nextLine());
         }
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
         System.out.println("Average: " + average);
         System.out.println("How many numbers: " + howMany);
         System.out.println("The sum is: " + sum);
