72209a4df8ac14a72b93275130b7b0de6118f54d
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-25 17:36:47 +0200

    Exercise 28

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        int sum = 0;
+        int number = 100;
 
-        while (true) {
-            int read = Integer.parseInt(reader.nextLine());
-            if (read == 0) {
-                break;
-            }
-            System.out.println("Sum now: " + (sum+= read));
+        while (number > 0) {
+            System.out.println(number);
+            number--;
         }
-        System.out.println("Sum in the end: " + sum);
     }
 }
