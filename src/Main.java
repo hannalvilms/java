7606f8f620650c94869a1592e1e236a183cadef7
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-25 17:48:42 +0200

    Exercise 30

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
         int number = 1;
+        System.out.print("Type a number: ");
+        int numberInput = Integer.parseInt(reader.nextLine());
 
-        while (number <= 100) {
-            if (number % 2 == 0) {
-                System.out.println(number);
-            }
+        while (number <= numberInput) {
+            System.out.println(number);
             number++;
         }
     }
