93031876db2748a9a64eb8840ce645d70b9c22c7
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-25 18:31:07 +0200

    Exercise 32

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.print("First number: ");
-        int firstNumber = Integer.parseInt(reader.nextLine());
-        System.out.print("Last number: ");
-        int lastNumber = Integer.parseInt(reader.nextLine());
-
-        while (firstNumber <= lastNumber) {
-            System.out.println(firstNumber);
-            firstNumber++;
+        int result = 0;
+        System.out.print("Sum until: ");
+        int number = Integer.parseInt(reader.nextLine());
+        int i = 1;
+        while (i <= number) {
+            result += i;
+            i++;
         }
+        System.out.println("Sum is: " + result);
     }
 }
