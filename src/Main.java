6ca522f1568faed4aee2cba57aabdd2a832ef01d
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-25 20:19:19 +0200

    Exercise 36.2

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,13 @@ public class Main {
 
         System.out.print("Type numbers: ");
         int number = Integer.parseInt(reader.nextLine());
+        int sum = 0;
 
         while (number != -1) {
+            sum += number;
             number = Integer.parseInt(reader.nextLine());
         }
+        System.out.println("The sum is: " + sum);
         System.out.println("Thank you and see you later!");
     }
 }
