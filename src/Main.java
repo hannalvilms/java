429c78f271ff11f2a8f9fa7fd552c731f0b7ee1a
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-25 20:30:06 +0200

    Exercise 36.4

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,15 @@ public class Main {
         int number = Integer.parseInt(reader.nextLine());
         int sum = 0;
         int howMany = 0;
+        double average = 0;
 
         while (number != -1) {
             sum += number;
             howMany++;
+            average = (double)sum / howMany;
             number = Integer.parseInt(reader.nextLine());
         }
+        System.out.println("Average: " + average);
         System.out.println("How many numbers: " + howMany);
         System.out.println("The sum is: " + sum);
         System.out.println("Thank you and see you later!");
