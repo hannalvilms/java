d9340824bc7df33c8b9eff1bbff2c1c12468e6ed
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-28 15:53:02 +0200

    Exercise 41.2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Guess a number: ");
-        int number = Integer.parseInt(reader.nextLine());
-        int rightNumber = 50;
 
-        if (number < rightNumber) {
-            System.out.println("The number is greater.");
-        } else if (number == rightNumber) {
-            System.out.println("Congratulations, your guess is correct!");
-        } else {
-            System.out.println("The number is lesser.");
-        }
+    public static void main(String[] args) {
+        drawNumber();
     }
 
+    public static void drawNumber() {
+        int number = (int)(Math.random()*101) ;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Guess a number: ");
+
+        int guess = Integer.parseInt(input.nextLine());
+
+        while (number != guess) {
+            if (guess < number) {
+                System.out.println("The number is greater.");
+            } else if (guess > number) {
+                System.out.println("The number is lesser.");
+            }
+            System.out.println("Guess a number: ");
+            guess = Integer.parseInt(input.nextLine());
+        }
+        System.out.println("Congratulations, your guess is correct!");
+    }
 }
 
