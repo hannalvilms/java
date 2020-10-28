f316065893013605be523f2c08ffc6640cc31185
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-28 21:45:29 +0200

    Exercise 47

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    Scanner reader = new Scanner(System.in);
-    public static double average(double number1, double number2, double number3, double number4) {
-        double numberAverage = (number1 + number2 + number3 + number4)/4;
-        return numberAverage;
+    public static int calculateCharacters(String text) {
+        return text.length();
     }
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter four numbers: ");
-        double numberOne = Integer.parseInt(reader.nextLine());
-        double numberTwo = Integer.parseInt(reader.nextLine());
-        double numberThree = Integer.parseInt(reader.nextLine());
-        double numberFour = Integer.parseInt(reader.nextLine());
-        double answer = average(numberOne, numberTwo, numberThree, numberFour);
-        System.out.println("Average: " + answer);
+        System.out.print("Type your name: ");
+        String yourName = reader.nextLine();
+
+        System.out.println("Number of characters: " + calculateCharacters(yourName));
     }
 }
 
