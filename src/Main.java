75eb6dbe28834b4e9352aba3829d8d057e0da12b
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-28 22:46:08 +0200

    Exercise 56

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,22 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String reverse(String text) {
+        String reverseText = "";
+        int i = text.length()-1;
+        while (i >= 0){
+            char symbol = text.charAt(i);
+            reverseText += symbol;
+            i--;
+        }
+        return reverseText;
+    }
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type the first word: ");
-        String firstWord = reader.nextLine();
-        System.out.print("Type the second word: ");
-        String secondWord = reader.nextLine();
-
-        int index = firstWord.indexOf(secondWord);
-
-        if (index == 1) {
-            System.out.println("The word '" + secondWord + "' is found in the word '" + firstWord + "'");
-        } else {
-            System.out.println("The word '" + secondWord + "' is not found in the word '" + firstWord + "'");
-        }
-
-
-
+        System.out.print("Type in your text: ");
+        String text = reader.nextLine();
+        System.out.println("In reverse order: " + reverse(text));
     }
 }
 
