abd68bcd8a198e2a15d95aa0656fe90e080802ee
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-28 15:56:01 +0200

    Exercise 41.3

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,15 @@ public class Main {
 
         int guess = Integer.parseInt(input.nextLine());
 
+        int guesses = 1;
+
         while (number != guess) {
             if (guess < number) {
-                System.out.println("The number is greater.");
+                System.out.println("The number is greater, guesses made: " + guesses);
             } else if (guess > number) {
-                System.out.println("The number is lesser.");
+                System.out.println("The number is lesser, guesses made: " + guesses);
             }
+            guesses++;
             System.out.println("Guess a number: ");
             guess = Integer.parseInt(input.nextLine());
         }
