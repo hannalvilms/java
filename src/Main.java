d8dbccbe0cfa1ef31b2f2b125f1c71bbe2a52145
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-28 14:27:07 +0200

    Exercise 39.1

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("How many? ");
-        int number = Integer.parseInt(reader.nextLine());
-        int i = 1;
-        while (true) {
-            if (i <= number) {
-                i++;
-                printText() ;
-            }
-        }
+        printStars(5);
+        printStars(3);
+        printStars(9);
     }
-    public static void printText() {
-        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    public static void printStars(int amount) {
+        int i = 0;
+        while (i < amount) {
+            System.out.println("*");
+            i++;
+        }
+        System.out.println();
     }
 }
