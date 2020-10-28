27bd314cd2a9a64a0784a4686f4f5b7eea8a3c9a
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-28 13:47:39 +0200

    Exercise 38

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        printText();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("How many? ");
+        int number = Integer.parseInt(reader.nextLine());
+        int i = 1;
+        while (true) {
+            if (i <= number) {
+                i++;
+                printText() ;
+            }
+        }
     }
     public static void printText() {
         System.out.println("In the beginning there were the swamp, the hoe and Java.");
