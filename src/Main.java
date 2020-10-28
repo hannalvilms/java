fbe52b133d2e6474b31dad17a273ddb62061cebd
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-28 14:54:19 +0200

    Exercise 41.1

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
-        printTriangle(4);
-    }
-    public static void printTriangle(int size) {
-        int row = 0;
-        while (row < size) {
-            int width = 0;
-            while (width <= row) {
-                System.out.println("*");
-                width++;
-            }
-            row++;
-            System.out.println();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Guess a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        int rightNumber = 50;
+
+        if (number < rightNumber) {
+            System.out.println("The number is greater.");
+        } else if (number == rightNumber) {
+            System.out.println("Congratulations, your guess is correct!");
+        } else {
+            System.out.println("The number is lesser.");
         }
     }
+
 }
+
