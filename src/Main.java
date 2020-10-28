816245b9f8c64adc5eeabdb2ad6ac70a1554379a
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-28 22:01:37 +0200

    Exercise 50

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static char lastCharacter(String text) {
-        char lastChar = text.charAt(text.length() -1 );
-        return lastChar;
-    }
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.print("Type your name: ");
         String yourName = reader.nextLine();
 
-        System.out.println("Last character: " + lastCharacter(yourName));
+        int i = 0;
+        if (yourName.length() >= 3) {
+            while (i < 3) {
+                System.out.println((i+1) + ". character: " + yourName.charAt(i));
+                i++;
+            }
+        }
     }
 }
 
