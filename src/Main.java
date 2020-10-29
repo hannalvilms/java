75e7b1f60ab02f748954e370fc5f950ecf3f0605
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-29 21:28:11 +0200

    Exercise 69

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,33 @@ import java.util.*;
 
 public class Main {
 
-    public static boolean moreThanOnce(ArrayList<Integer> list, int number) {
-        int count = 0;
-        for (int numberInList : list) {
-            if (numberInList == number) {
-                count = count +1;
-            }
-            if (count > 1) {
-                return true;
-            }
+    public static String reverse(String text) {
+        String reversed = "";
+        int i = 0;
+        while (i < text.length()) {
+            char c = text.charAt(i);
+            reversed = c + reversed;
+            i++;
         }
-        return false;
+        return reversed;
+    }
+
+    public static boolean palindrome(String text) {
+        boolean isPalindrome = text.equals(reverse(text));
+        return isPalindrome;
     }
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(3);
-        list.add(2);
-        list.add(7);
-        list.add(2);
-
-        System.out.println("Type a number: ");
-        int number = Integer.parseInt(reader.nextLine());
-        if (moreThanOnce(list, number)) {
-            System.out.println(number + " appears more than once.");
+        System.out.println("Type a text: ");
+        String text = reader.nextLine();
+        if (palindrome(text)) {
+            System.out.println("The text is a palindrome!");
         } else {
-            System.out.println(number + " does not appear more than once.");
+            System.out.println("The text is not a palindrome!");
         }
     }
+
 }
 
