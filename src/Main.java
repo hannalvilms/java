00badacf8df4ed110ee5047c74dfc50fed8e4aea
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-30 12:33:32 +0200

    Exercise 73

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,9 @@ import java.util.*;
 
 public class Main {
 
-    public static void smartCombine(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        for (Integer number : list2) {
-            if (!list1.contains(number)) {
-                list1.add(number);
-            }
-        }
-    }
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
-        ArrayList<Integer> list2 = new ArrayList<Integer>();
-
-        Collections.addAll(list1, 1, 2, 3, 2, 4);
-        Collections.addAll(list2, 3, 4, 5);
-
-        smartCombine(list1, list2);
-        System.out.println(list1);
-        System.out.println(list2);
+        Product i = new Product("Banana", 0.7, 2);
+        i.printProduct();
     }
-
 }
 
