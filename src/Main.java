6c451a869f9460d6f91a00953fd82d311da6320b
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-31 21:00:30 +0200

    Exercise 87.2

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main {
         Apartment twoRoomsBrooklyn = new Apartment(2, 38, 4200);
         Apartment fourAndKitchenBronx = new Apartment(3, 78, 2500);
 
-        System.out.println( studioManhattan.larger(twoRoomsBrooklyn) );       // false
-        System.out.println( fourAndKitchenBronx.larger(twoRoomsBrooklyn) );   // true
+        System.out.println( studioManhattan.priceDifference(twoRoomsBrooklyn) );
+        System.out.println( fourAndKitchenBronx.priceDifference(twoRoomsBrooklyn) );
     }
 }
 
