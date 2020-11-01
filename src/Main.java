8f6014d43dd4d054b0840af58172d520b330742f
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-11-01 20:35:24 +0200

    Exercise 101.1

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        NightSky NightSky = new NightSky(0.1, 40, 10);
-        NightSky.print();
-        System.out.println("Number of stars: " + NightSky.starsInLastPrint());
-        System.out.println("");
+        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
+        System.out.println(cheese.title());
+        System.out.println(cheese.publisher());
+        System.out.println(cheese.year());
 
-        NightSky = new NightSky(0.2, 15, 6);
-        NightSky.print();
-        System.out.println("Number of stars: " + NightSky.starsInLastPrint());
+        System.out.println(cheese);
 
     }
 }
