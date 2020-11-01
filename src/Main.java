03036807ce3897a4d4fdfa7a226271cb1e5a5262
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-11-01 22:30:22 +0200

    Exercise 105.2

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         GuessingGame game = new GuessingGame();
+        System.out.println(game.average(3, 4));
 
-        System.out.println(game.isGreaterThan(32));
     }
 }
 
