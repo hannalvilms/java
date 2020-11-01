523dafc338679c9bbaf858eb42eeb68465bdc009
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-11-01 18:29:13 +0200

    Exercise 98.2

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
         int[] original = {1, 2, 3, 4};
-        int[] copied = copy(original);
-
-        // change the copied
-        copied[0] = 99;
+        int[] reverse = reverseCopy(original);
 
         // print both
-        System.out.println( "original: " + Arrays.toString(original));
-        System.out.println( "copied: " + Arrays.toString(copied));
+        System.out.println( "original: " +Arrays.toString(original));
+        System.out.println( "reversed: " +Arrays.toString(reverse));
     }
 
     public static int[] copy(int[] array) {
-        int[] newArray = new  int[array.length];
+        int[] newArray = new int[array.length];
         for (int i = 0; i < array.length; i++) {
             newArray[i] = array[i];
+        }
+        return newArray;
+    }
+
+    public static int[] reverseCopy(int[] array) {
+
+        int[] newArray = new int[array.length];
+
+        for (int i = array.length-1, j = 0; i >= 0; i--) {
+            newArray[j] = array[i];
+            j++;
         }
         return newArray;
     }
