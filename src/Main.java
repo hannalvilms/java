ec3c997ed037ccc66de75b236b96818aaac6f04c
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-11-02 14:28:30 +0200

    Java alused kontrolltöö - Ülesanne 1

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        GuessingGame game = new GuessingGame();
+        int number = 10;
 
-        game.play(1,10);
+        for (int row = 0; row < number; row++) {
 
+            for (int col = 0; col < number; col++) {
+                if (row > col) {
+                    System.out.println(number - row - 1 + " ");
+                } else {
+                    System.out.println(number - col - 1 + " ");
+                }
+            }
+            //reavahetus
+            System.out.println();
+        }
     }
 }
 
