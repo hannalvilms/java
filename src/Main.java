2964290719358eb5cddd97f33b188f7ad28e8274
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-11-18 16:18:13 +0200

    W2 - Excerise 7.4

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Dictionary dictionary = new Dictionary();
-        dictionary.add("apina", "monkey");
-        dictionary.add("banaani", "banana");
-        ArrayList<String> translations = dictionary.translationList();
-        for(String translation: translations) {
-            System.out.println(translation);
-        }
+        Scanner reader = new Scanner(System.in);
+        Dictionary dict = new Dictionary();
+
+        TextUserInterface ui = new TextUserInterface(reader, dict);
+        ui.start();
     }
 }
 
---
+++ src/TextUserInterface.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class TextUserInterface {
+
+    public Scanner reader;
+    public Dictionary dictionary;
+
+    public TextUserInterface(Scanner reader, Dictionary dictionary){
+        this.reader = reader;
+        this.dictionary = dictionary;
+    }
+    public void start(){
+        System.out.println("Statement: ");
+        System.out.println("    quit - quit the text user interface");
+
+        while (true) {
+            System.out.print("Statement: ");
+            String input = reader.nextLine();
+            if (input.equals("quit")) {
+                System.out.println("Cheers!");
+                break;
+            } else {
+                System.out.println("Unknown statement!");
+            }
+        }
+
+    }
+}
