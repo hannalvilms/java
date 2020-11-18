f56838016a50688a816d4bb9112eae752349a899
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-11-18 22:57:00 +0200

    W2 - Excerise 9.2

---
+++ src/VehicleRegister.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class VehicleRegister {
+
+    public HashMap<RegistrationPlate, String> owners;
+
+    public boolean add(RegistrationPlate plate, String owner){
+        if (owners.containsKey(plate)) {
+            return false;
+        }
+        owners.put(plate, owner);
+        return true;
+    }
+    public String get(RegistrationPlate plate) {
+        if (!owners.containsKey(plate)){
+            return null;
+        }
+        return owners.get(plate);
+    }
+    public boolean delete(RegistrationPlate plate) {
+        if (!owners.containsKey(plate)) {
+            return false;
+        }
+        owners.remove(plate);
+        return true;
+    }
+}
