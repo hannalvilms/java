e6cd36e8cb7fcb9a20b27d9755dee17f3515b7c5
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-11-25 17:56:07 +0200

    W2 - Excerise 21

---
+++ src/Printer.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Printer {
+
+    private Scanner reader;
+    private File file;
+
+    public Printer(String fileName) throws Exception{
+        this.file = new File(fileName);
+    }
+
+    public void printLinesWhichContain(String word) throws Exception {
+        this.reader = new Scanner(file);
+        if (word.equals("")) {
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                System.out.println(line);
+            }
+        }
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+            if (line.contains(word)) {
+                System.out.println(line);
+            }
+        }
+    }
+}
